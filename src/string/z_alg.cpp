b3634914e865e5b884d12d720f0f46cf1a510a5f
PringDeSu <ianneeddiamond@gmail.com>
2025-09-01 14:58:34 +0800

    brand-new Z_alg

--- src/string/z_alg.cpp
+++ src/string/z_alg.cpp
@@ -1,22 +1,11 @@
 template <typename T>
 struct Z_alg {
     void operator()(T a, int n, int *z) {
-        fill(z, z + n + 1, 0);
+        z[0] = 0;
         int l = 0;
         for (int i = 1; i <= n; i++) {
-            if (i >= l + z[l]) {
-                while (i + z[i] < n && a[i + z[i]] == a[z[i]]) z[i]++;
-                l = i;
-                continue;
-            }
-            int i_ = i - l;
-            if (i_ + z[i_] == z[l]) {
-                z[i] = z[i_];
-                while (i + z[i] < n && a[i + z[i]] == a[z[i]]) z[i]++;
-                l = i;
-                continue;
-            }
-            z[i] = (i_ + z[i_] < z[l] ? z[i_] : z[l] - i_);
+            for (z[i] = max(0, min(z[i - l], l + z[l] - i)); i + z[i] < n && a[i + z[i]] == a[z[i]]; z[i]++);
+            if (i + z[i] > l + z[l]) l = i;
         }
     }
 };
